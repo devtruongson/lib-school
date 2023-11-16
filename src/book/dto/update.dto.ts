c990d5d211565afacdede966147c7007b5333334
devtruongson <truongsonpt.80@gmail.com>
2023-11-16 22:17:46 +0700

    update

--- src/book/dto/update.dto.ts
+++ src/book/dto/update.dto.ts
@@ -1,4 +1,4 @@
-import { IsArray, IsBoolean, IsNotEmpty, IsString, Min } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 import { createDTO } from './create.dto';
 
 export class updateDTO extends createDTO {
